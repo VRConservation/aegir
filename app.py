0c3e5f39886eb6729d180399b51704b820193f3f
VRConservation <vance@3point.xyz>
2026-08-19 16:03:35 +0100

    Bump version: 0.1.0 → 0.2.0

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """
 Aegir - Kayak Journey Planner
 """
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from flask import Flask, jsonify, request, render_template
 from datetime import datetime, timedelta
